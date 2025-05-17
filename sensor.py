b79f914420e53e7f0ad56a39eb4956d50a8088ae
WoodyPride1970 <wordy.rewind.7r@icloud.com>
2025-05-18 00:38:58 +0900

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -15,28 +15,23 @@ class SensorBase:
     def get_data(self):
         raise NotImplementedError()
 
-class RealSensor(SensorBase):
+class RealSensor:
     def __init__(self):
-        self.x_est_lat, self.p_est_lat = 0.0, 1.0
-        self.x_est_lon, self.p_est_lon = 0.0, 1.0
+        self.imu = IMUReader()
+        self.x_est_lat, self.p_est_lat = 35.681236, 1.0
+        self.x_est_lon, self.p_est_lon = 139.767125, 1.0
 
-    def read_gnss(self):
-        # 実際のGNSSデータ取得処理に置き換える
-        return 35.681236 + random.uniform(-0.0001, 0.0001), 139.767125 + random.uniform(-0.0001, 0.0001)
-
-    def read_heading(self):
-        # 実際のIMUによる方位角取得処理に置き換える
-        return (random.uniform(0, 360) + 90) % 360  # +90度補正
-
-    def read_rtk_status(self):
-        # 例: u-blox の Fix type 1=none, 4=RTK Fixed
-        return random.choice([1, 4])
-
-    def get_data(self):
-        raw_lat, raw_lon = self.read_gnss()
-        heading = self.read_heading()
-        rtk_fix_type = self.read_rtk_status()
+    def read_data(self):
+        raw_lat = 35.681236 + random.uniform(-0.0002, 0.0002)
+        raw_lon = 139.767125 + random.uniform(-0.0002, 0.0002)
+        rtk_fix_type = random.choice([1, 4])
         error_radius = random.uniform(0.2, 1.5) if rtk_fix_type == 4 else random.uniform(1.5, 5.0)
+
+        heading = 0.0
+        if self.imu.is_connected():
+            heading = self.imu.read_heading()
+        else:
+            heading = random.uniform(0, 360)
 
         self.x_est_lat, self.p_est_lat = kalman_filter(raw_lat, self.x_est_lat, self.p_est_lat)
         self.x_est_lon, self.p_est_lon = kalman_filter(raw_lon, self.x_est_lon, self.p_est_lon)
@@ -44,12 +39,12 @@ class RealSensor(SensorBase):
         return {
             "lat": self.x_est_lat,
             "lon": self.x_est_lon,
-            "heading": heading,
+            "heading": heading + 90,
             "rtk_fix_type": rtk_fix_type,
             "error_radius": error_radius,
             "timestamp": datetime.utcnow().isoformat() + "Z"
-        }
-
+}
+            
 class SensorSimulator(SensorBase):
     def __init__(self):
         self.x_est_lat, self.p_est_lat = 35.681236, 1.0
